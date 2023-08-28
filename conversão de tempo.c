62f2f03cf9986d91acbcb12311393034d6e77fbd
O-Gui <137916326+O-Gui@users.noreply.github.com>
2023-08-28 01:57:17 -0300

    conversão de tempo

---
+++ conversão de tempo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+    int main() {
+
+    int segundos;
+    int horas;
+    int minutos;
+
+    scanf("%d", &segundos);
+
+    horas = segundos / 3600;
+    segundos %= 3600;
+    minutos = segundos / 60;
+    segundos %= 60;
+
+   
+    printf("%d:%d:%d\n", horas, minutos, segundos);
+
+
+  return 0;
+}
