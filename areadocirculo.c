65f8dbac2fde4f13e0f4ead6caf62094560faf19
O-Gui <137916326+O-Gui@users.noreply.github.com>
2023-08-29 21:35:00 -0300

    areadocirculo

---
+++ areadocirculo.c
@@ -0,0 +1,16 @@
+// link do beecrowd https://www.beecrowd.com.br/judge/en/runs/code/35253554
+
+#include <stdio.h>
+
+int main() {
+    
+    double raio;
+
+    scanf("%lf", &raio);
+
+    double A = 3.14159 * raio * raio;
+
+    printf("A=%.4lf\n", A);
+
+    return 0;
+}
