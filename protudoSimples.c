a90493b9613fc7e5c6f8088d427395a6d162e497
O-Gui <137916326+O-Gui@users.noreply.github.com>
2023-08-29 21:25:59 -0300

    PROD

---
+++ protudoSimples.c
@@ -0,0 +1,20 @@
+// link do beecrowd https://www.beecrowd.com.br/judge/en/runs/code/35253219
+
+#include <stdio.h>
+
+int main() {
+    int a;
+	int b;
+    int  PROD;
+
+   scanf("%d", &a);
+   scanf("%d", &b);
+
+  
+    PROD = a * b;
+
+  
+    printf("PROD = %d\n",  PROD);
+
+    return 0;
+}
